7e8d8a5b6dd629eb97bf709fbf4884d5f527b8d8
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-12-05 20:03:48 +0200

    Fetching repositories from backend enabled

---
+++ app/Main.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, View } from "react-native";
+import { Route, Routes } from "react-router-native";
+
+import AppBar from "./components/AppBar";
+import RepositoryList from "./components/RepositoryList";
+import SignIn from "./components/SignIn";
+
+import theme from "./theme";
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+    backgroundColor: theme.colors.background,
+  },
+});
+
+export default function Main() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,16 @@
-import { StyleSheet, View } from "react-native";
-import { NativeRouter, Route, Routes } from "react-router-native";
+import { NativeRouter } from "react-router-native";
 
-import AppBar from "./components/AppBar";
-import RepositoryList from "./components/RepositoryList";
-import SignIn from "./components/SignIn";
+import Main from "./Main";
+//import createApolloClient from "./utils/apolloClient";
 
-import theme from "./theme";
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    flexShrink: 1,
-    backgroundColor: theme.colors.background,
-  },
-});
+//const apolloClient = createApolloClient();
 
 export default function Index() {
   return (
     <NativeRouter>
-      <View style={styles.container}>
-        <AppBar />
-        <Routes>
-          <Route path="/" element={<RepositoryList />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      </View>
+      {/* <ApolloProvider client={apolloClient}> */}
+        <Main />
+      {/* </ApolloProvider> */}
     </NativeRouter>
   );
 }
