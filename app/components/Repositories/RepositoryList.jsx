c54a3ab7599c155295023f2129759b992a4c92fb
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-12-08 14:38:31 +0200

    10.24

--- app/components/Repositories/RepositoryList.jsx
+++ app/components/Repositories/RepositoryList.jsx
@@ -1,4 +1,6 @@
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 import { Link } from 'react-router-native';
 import useRepositories from '../../hooks/useRepositories';
 import useSorter from '../../hooks/useSorter';
@@ -8,40 +10,71 @@ import RepositoryItem from './RepositoryItem';
 const styles = StyleSheet.create({
   separator: {
     height: 10,
-  },
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryListContainer = ({ repositories }) => {
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
+const RepositoryListHeader = ({ searchKeyword, setSearchKeyword }) => (
+  <Searchbar
+    placeholder="Search repositories"
+    value={searchKeyword}
+    onChangeText={setSearchKeyword}
+    style={{ margin: 10, marginBottom: 0, backgroundColor: 'white' }}
+  />
+);
 
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => (
-        <Link to={`/repository/${item.id}`}>
-          <RepositoryItem item={item} />
-        </Link>
-      )}
-      keyExtractor={item => item.id}
-    />
-  );
+export class RepositoryListContainer extends React.Component {
+
+  render() {
+    const { repositories, searchKeyword } = this.props;
+    const repositoryNodes = repositories
+      ? repositories.edges.map((edge) => edge.node)
+      : [];
+
+    const filteredNodes = repositoryNodes.filter((repo) =>
+      repo.fullName.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
+
+    return (
+      <FlatList
+        data={filteredNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => (
+          <Link to={`/repository/${item.id}`}>
+            <RepositoryItem item={item} />
+          </Link>
+        )}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={this.renderHeader}
+      />
+    );
+  }
 }
-
-export { RepositoryListContainer };
 
 const RepositoryList = () => {
   const { sorter, setSorter, orderBy, orderDirection } = useSorter();
-  const { repositories } = useRepositories({ orderBy, orderDirection });
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [debouncedKeyword] = useState(searchKeyword);
+  
+  const { repositories } = useRepositories({ 
+    orderBy, 
+    orderDirection,
+    searchKeyword: debouncedKeyword
+  });
 
   return (
     <View>
+      <RepositoryListHeader
+        searchKeyword={searchKeyword}
+        setSearchKeyword={setSearchKeyword}
+      />
       <Sorter sorter={sorter} setSorter={setSorter} />
-      <RepositoryListContainer repositories={repositories} />
+      <RepositoryListContainer 
+        repositories={repositories} 
+        searchKeyword={searchKeyword} 
+        setSearchKeyword={setSearchKeyword} 
+      />
     </View>
   );
 };
